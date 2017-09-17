3544117dd3e03d3d7ef25eb2afee36cc592bc407
Nate Wellek <nwellek@umich.edu>
2017-09-17 16:41:34 -0400

    HW Number 1

--- SI364-HW1.py
+++ SI364-HW1.py
@@ -2,7 +2,8 @@
 ## SI 364 F17
 ## Due: September 19, 2017
 ## 500 points
-
+import requests
+import json
 ## PART 1 - 100 points
 
 ## First, set up a new-to-this-assignment conda environment. To the Canvas assignment, you should submit:
@@ -12,24 +13,35 @@
 ######
 
 ## [PROBLEM 1] - 150 points
-## Below is code for one of the simplest possible Flask applications. Edit the code so that once you run this application locally and go to the URL 'http://localhost:5000/class', you see a page that says "Welcome to SI 364!"
+## Below is code for one of the simplest possible Flask applications. 
+#Edit the code so that once you run this application locally and go to the URL 'http://localhost:5000/class',
+# you see a page that says "Welcome to SI 364!"
 
 from flask import Flask
 app = Flask(__name__)
 app.debug = True
 
-@app.route('/')
+@app.route('/class')
 def hello_to_you():
-    return 'Hello!'
-
-
-if __name__ == '__main__':
-    app.run()
+    return 'Welcome to SI 364!!'
 
 
 ## [PROBLEM 2] - 250 points
-## Edit the code chunk above again so that if you go to the URL 'http://localhost:5000/movie/<name-of-movie-here-one-word>' you see a big dictionary of data on the page. For example, if you go to the URL 'http://localhost:5000/movie/ratatouille', you should seesomething like the data shown in the included file sample_ratatouille_data.txt, which contains data about the animated movie Ratatouille. However, if you go to the url http://localhost:5000/movie/titanic, you should get different data, and if you go to the url 'http://localhost:5000/movie/dsagdsgskfsl' for example, you should see data on the page that looks like this:
+## Edit the code chunk above again so that if you go to the URL 
+#'http://localhost:5000/movie/<name-of-movie-here-one-word>' 
+#you see a big dictionary of data on the page. For example, 
+#if you go to the URL 'http://localhost:5000/movie/ratatouille', 
+#you should seesomething like the data shown in the included file sample_ratatouille_data.txt, 
+#which contains data about the animated movie Ratatouille. However, 
+#if you go to the url http://localhost:5000/movie/titanic, y
+#ou should get different data, and if you go to the url 'http://localhost:5000/movie/dsagdsgskfsl' for example, 
+#you should see data on the page that looks like this:
 
+@app.route('/movie/<name>')
+def movie(name):
+	var = {'term': name, "media":"movie"}
+	url = requests.get('https://itunes.apple.com/search?', params = var)
+	return url.text
 # {
 #  "resultCount":0,
 #  "results": []
@@ -41,3 +53,6 @@ if __name__ == '__main__':
 ## Of course, you'll also need the requests library and knowledge of how to make a request to a REST API for data.
 
 ## Run the app locally (repeatedly) and try these URLs out!
+
+if __name__ == '__main__':
+    app.run()
